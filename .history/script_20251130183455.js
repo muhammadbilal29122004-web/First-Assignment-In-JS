a184e5cd00beaa7274d7616ada2bacb43691f17c
shahzaebkalam <shahzebkalam456@gmail.com>
2025-11-30 18:37:21 +0500

    first ass in js

---
+++ .history/script_20251130183455.js
@@ -0,0 +1,17 @@
+// Step 1: Parentheses
+const step1 = 3 + 3;       // 6
+const step2 = 2 + 1;       // 3
+
+// Step 2: Multiplication inside brackets
+const step3 = 5 * step2;   // 5*3 = 15
+
+// Step 3: Braces
+const step4 = 12 - step3;  
+
+// Step 4: Division and multiplication
+const step5 = 18 / step1 * 4; // 18/6*4 = 12
+
+// Step 5: Combine with braces and exponent
+const step6 = step5 - step4 + 7 ** 2; // 12 - (-3) + 49 = 64
+
+console.log(step6); // 64
